e81254ba5dee0ce27038dd202bde85b695dfa09e
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-20 16:26:58 -0700

    Added icons to expand and shopping cart as well as name and description on product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Product } from "@/lib/types";
 import IconButton from "@/components/ui/icon-button";
+import { Expand, ShoppingCart } from "lucide-react";
 
 interface ProductCard {
   data: Product;
@@ -11,7 +12,7 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   return (
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
-      {/* images and actions */}
+      {/* Images and Actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           alt="image"
@@ -19,11 +20,27 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
           fill
           className="aspect-square object-cover rounded-md"
         />
-        <div className="opacity-0 group-hover:opacity-100">
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton />
+            <IconButton
+              onClick={() => {}}
+              icon={<Expand size={20} className="text-gray-600" />}
+            />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
           </div>
         </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
       </div>
     </div>
   );
