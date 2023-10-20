5ae887fa910ec4a318454102eafd14cca670dc31
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-20 16:23:15 -0700

    Created reusable IconButton component

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { Product } from "@/lib/types";
+import IconButton from "@/components/ui/icon-button";
 
 interface ProductCard {
   data: Product;
@@ -12,7 +13,17 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
     <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* images and actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <Image alt="image" src={data?.images?.[0].url} fill />
+        <Image
+          alt="image"
+          src={data?.images?.[0].url}
+          fill
+          className="aspect-square object-cover rounded-md"
+        />
+        <div className="opacity-0 group-hover:opacity-100">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton />
+          </div>
+        </div>
       </div>
     </div>
   );
