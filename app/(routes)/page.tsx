5710b0a85973fdb729c7d1147202d0882d9c3cd5
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-20 16:01:28 -0700

    Created action to get billboard which will display the billboard name and image on the homepage of the store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <div>Hello store</div>;
-}
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboards";
+
+const Home = async () => {
+  const billboard = await getBillboard("4cbfda3b-8487-4e13-b41b-096a7826dfb9");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
+export default Home;
