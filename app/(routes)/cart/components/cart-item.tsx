3029e60c49c7dd86a10cbfeeaf0c18d2f6403559
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-10-21 13:08:25 -0700

    Created functionality to remove item and added refresh to page so item is removed

--- app/(routes)/cart/components/cart-item.tsx
+++ app/(routes)/cart/components/cart-item.tsx
@@ -8,12 +8,21 @@ import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
 import { Product } from "@/lib/types";
+import { useRouter } from "next/navigation";
 
 interface CartItemProps {
   data: Product;
 }
 
 const CartItem: React.FC<CartItemProps> = ({ data }) => {
+  const cart = useCart();
+  const router = useRouter();
+
+  const onRemove = () => {
+    cart.removeItem(data.id);
+    router.refresh();
+  };
+
   return (
     <li className="flex py-6 border-b">
       <div className="relative h-24 w-24 rounded-md overflow-hidden sm:h-48 sm:w-48">
@@ -26,7 +35,7 @@ const CartItem: React.FC<CartItemProps> = ({ data }) => {
       </div>
       <div className="relative ml-4 flex flex-1 flex-col justify-between sm:ml-6">
         <div className="absolute z-10 right-0 top-0">
-          <IconButton onClick={() => {}} icon={<X size={15} />} />
+          <IconButton onClick={onRemove} icon={<X size={15} />} />
         </div>
         <div className="relative pr-9 sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
           <div className="flex justify-between">
